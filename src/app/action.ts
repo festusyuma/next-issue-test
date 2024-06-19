dead916ab20603fb4e3fdbcebb3ed6da48b9aa21
Festus <festusyuma@gmail.com>
2024-06-19 14:12:29 +0100

    remove redirect from action

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,9 +1,5 @@
 'use server'
 
-import {redirect} from "next/navigation";
-import { v4 } from "uuid";
-
 export async function action() {
     await new Promise(resolve => setTimeout(resolve, 2000))
-    return redirect(`/?hello=${v4()}`)
 }
